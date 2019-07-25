7f693fbbd7b8c989af772696bd3ad00016597f32
Adam <lehreradam@yahoo.com>
2019-07-25 16:41:17 -0700

    Updates

---
+++ precache-manifest.b489c199e16b9b5844d6003cac5bfa0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c642f08752ff93e95bef0a802888da04",
+    "url": "/Clickers/index.html"
+  },
+  {
+    "revision": "42f761504f92d807af68",
+    "url": "/Clickers/static/css/main.d29522f9.chunk.css"
+  },
+  {
+    "revision": "008b3e8efd6e7295fcc6",
+    "url": "/Clickers/static/js/2.675a6941.chunk.js"
+  },
+  {
+    "revision": "42f761504f92d807af68",
+    "url": "/Clickers/static/js/main.c47e1664.chunk.js"
+  },
+  {
+    "revision": "c0e28664146f933becb6",
+    "url": "/Clickers/static/js/runtime~main.06fc3c11.js"
+  }
+]);
